080bb14f785d8df4ec75b3ffe348922eafe8044a
Victor Perron <victor@iso3103.net>
2019-02-27 00:39:17 +0100

    Very first rendering of the landing page

--- src/pages/landing.vue
+++ src/pages/landing.vue
@@ -1,5 +1,25 @@
 <template lang="pug">
-span Hello world
+div
+  #region-map
+  #disclaimer
+    .content
+      .row
+        .col.laius
+          span.olivier "
+          span Nous éprouvons un réel plaisir à manger et boire des aliments qui ont du goût, une histoire...
+          p Et ce plaisir est encore plus grand lorsque ces produits nous sont vendus par ceux qui les ont faits.
+          p.olivier C'est pourquoi ON AIME les magasins de producteurs."
+        .col.images
+          img(
+            alt='vache',
+            title='vache',
+            src='~src/assets/images/carottes.png'
+          )
+          img(
+            alt='carottes',
+            title='carottes',
+            src='~src/assets/images/vache_accueil.png'
+          )
 </template>
 
 <script>
@@ -17,5 +37,33 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "src/variables";
+
+#region-map {
+  height: 380px;
+  background-color: $purple;
+}
+
+@media (min-width: 1024px) {
+  .laius {
+    padding-top: 2rem;
+    width: 50%;
+    font-size: 1.5rem;
+    color: $deep-purple;
+  }
+  .images {
+    width: 50%;
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    img {
+    }
+  }
+}
+
+.olivier {
+  font-family: 'Olivier';
+  font-size: 3rem;
+}
 </style>
--- src/pages/root.vue
+++ src/pages/root.vue
@@ -1,6 +1,48 @@
 <template lang="pug">
 div
+  #header
+    .content
+      .row
+        .col.logo
+          a(href="/")
+            img(
+              id="top_logo"
+              src="~/src/assets/images/logo.png"
+              alt="Magasin de Producteurs"
+              title="Magasin de Producteurs"
+            )
+        .col.mentions
+          a(href='#')
+            img(
+              alt='Histoires de Producteurs',
+              title='Histoires de Producteurs',
+              src='~/src/assets/images/footer_catalog.png'
+            )
+            | Histoires de Producteurs
+          a(href='#')
+            img(
+              alt='Si vous êtes un Magasin de Producteurs',
+              title='Si vous êtes un Magasin de Producteurs',
+              src='~src/assets/images/home.png'
+            )
+            | Si vous êtes un Magasin de Producteurs
+          a(href='#')
+            img(
+              alt='Se Connecter',
+              title='Se Connecter',
+              src='~src/assets/images/logon.png'
+            )
+            | Se connecter
+          a(href='#')
+            img(
+              alt="S'inscrire",
+              title="S'inscrire",
+              src='~src/assets/images/subscribe.png'
+            )
+            | S'inscrire
+
   router-view
+  #footer
 </template>
 
 <script>
@@ -21,5 +63,46 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "src/variables";
+
+#top_logo {
+  margin: 6px;
+  float: left;
+}
+
+#header {
+  color: white;
+  position: relative;
+  min-height: 100px;
+  background-color: $deep-purple;
+  margin-bottom: 0px;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  .mentions a {
+    margin-left: 12px;
+    img {
+      margin-right: 6px;
+    }
+  }
+}
+
+@media (max-width: 1024px) {
+  .logo { width: 100%; }
+  .mentions { width: 100%; }
+}
+
+@media (min-width: 1024px) {
+  .logo { width: 30%; }
+  .mentions {
+    width: 70%;
+    position: absolute;
+    right: 0px;
+    bottom: 0px;
+  }
+}
 </style>
